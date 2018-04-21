27fadcf4521b0459975d6b9b02a68181c44b791f
pacien <pacien.trangirard@pacien.net>
2018-04-21 16:02:14 +0200

    Make instruction parser case insensitive

--- parser.ml
+++ parser.ml
@@ -13,12 +13,14 @@ let rec string_of_file f =
 
 let rec program_of_lex = function
   | [] -> []
-  | "zero" :: arg_1 :: tail -> (Zero (int_of_string arg_1)) :: (program_of_lex tail)
-  | "succ" :: arg_1 :: tail -> (Succ (int_of_string arg_1)) :: (program_of_lex tail)
-  | "copy" :: arg_1 :: arg_2 :: tail -> (Copy ((int_of_string arg_1), (int_of_string arg_2))) :: (program_of_lex tail)
-  | "jump" :: arg_1 :: arg_2 :: arg_3 :: tail -> (Jump ((int_of_string arg_1), (int_of_string arg_2), (int_of_string arg_3))) :: (program_of_lex tail)
-  | _ -> raise Syntax_error
+  | instr :: tail -> match (String.lowercase_ascii instr) :: tail with
+    | "zero" :: arg_1 :: tail -> (Zero (int_of_string arg_1)) :: (program_of_lex tail)
+    | "succ" :: arg_1 :: tail -> (Succ (int_of_string arg_1)) :: (program_of_lex tail)
+    | "copy" :: arg_1 :: arg_2 :: tail -> (Copy ((int_of_string arg_1), (int_of_string arg_2))) :: (program_of_lex tail)
+    | "jump" :: arg_1 :: arg_2 :: arg_3 :: tail -> (Jump ((int_of_string arg_1), (int_of_string arg_2), (int_of_string arg_3))) :: (program_of_lex tail)
+    | _ -> raise Syntax_error
 
 let program_of_string str =
   let lex = Str.split (Str.regexp "[\t\n(),]+") str
   in List.iter (fun s -> print_string s; print_newline ()) lex; program_of_lex lex
+
